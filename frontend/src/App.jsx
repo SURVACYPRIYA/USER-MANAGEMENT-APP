df006fc0afd7ed845831ade9e777f09b564d1d1b
Priya <23eg105n55@anurag.edu.in>
2026-04-19 11:23:53 +0530

    fix: missing axios dependency causing build failure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,42 @@
-import { createBrowserRouter, RouterProvider } from "react-router"
-import RootLayout from "./components/RootLayout"
-import Home from "./components/Home"
-import UserList from "./components/UserList"
-import User from "./components/User"
-import AddUser from "./components/AddUser"
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import RootLayout from "./components/RootLayout";
+import Home from "./components/Home";
+import UserList from "./components/UserList";
+import User from "./components/User";
+import AddUser from "./components/AddUser";
 
 function App() {
-  const routerObj=createBrowserRouter([
+  const routerObj = createBrowserRouter([
     {
-      path:"/",
-      element:<RootLayout />,
-      children:[
+      path: "/",
+      element: <RootLayout />,
+      children: [
         {
-          path:"home",
-          element:<Home />,
+          index: true,
+          element: <Home />
         },
         {
-          path:"add-user",
-          element:<AddUser />,
+          path: "home",
+          element: <Home />
         },
         {
-          path:"user",
-          element:<User />,
+          path: "add-user",
+          element: <AddUser />
         },
         {
-          path:"users-list",
-          element:<UserList />,
+          path: "user",
+          element: <User />
+        },
+        {
+          path: "users-list",
+          element: <UserList />
         }
-      ],
-    },
-  ])
+      ]
+    }
+  ]);
+
   return <RouterProvider router={routerObj} />;
 }
 
-export default App
+export default App;
